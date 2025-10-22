720c6ef8379d5b8ac1c88535efe68ec356fe4356
xiaziyi36 <18171700006@163.com>
2025-10-22 17:32:21 +0800

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,6 @@
 import pytest
 
-os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
 from app.app import dedupe, add
 def test_dedupe():
     """测试去重功能"""
